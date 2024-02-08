ff2fa603d80a6cc10b4a3ddc5c3e6fe53a423dce
nilloferreiira <danillodana@outlook.com>
2024-02-07 22:47:40 -0300

    responsividade

--- src/app.tsx
+++ src/app.tsx
@@ -57,7 +57,7 @@ export function App() {
   }
 
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={Logo} alt="NLW Expert" />
 
       <form className="w-full ">
@@ -79,7 +79,7 @@ export function App() {
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreated={onNoteCreated} />
         {filteredNotes.map((note: any) => {
           return <NoteCard key={note.id} note={note} deleteNote={deleteNote} />;
